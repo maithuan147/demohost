ca8d90243cd8d00c0033c2a283b45d0f921b22c2
unknown <maithuan147>
2020-04-24 20:11:35 +0700

    1

--- js.js
+++ js.js
@@ -1,5 +1,34 @@
 let file = document.getElementById('file');
 file.onchange = () => {
-    srcImage = URL.createObjectURL(file.files[0]);
-    document.getElementById('myImg').src = srcImage;
+    let fileUpload = file.files;
+    let dem = fileUpload.length;
+    renderImg(dem);
 }
+
+function deleteImg(){
+    let fileUpload = file.files;
+    let fileUploadArray = Array.prototype.slice.call(fileUpload);
+    let deletes = document.getElementsByClassName('delete');
+    let deleteArray = Array.prototype.slice.call(deletes);
+    deleteArray.forEach(element => {
+        element.onclick = () => {
+            let Id = element.getAttribute('data-id');
+            fileUploadArray.splice(Id,1);
+            let dem = fileUploadArray.length;
+            console.log(fileUploadArray);
+            renderImg(dem);
+        }
+    });
+}
+
+function renderImg(dem){
+    let render = document.getElementById('render');
+    for (let i = 0; i < dem; i++) {
+        srcImage = URL.createObjectURL(file.files[i]);
+        render.innerHTML += `<img id="myImg" src="${srcImage}" alt="your image"><span class="delete" data-id=${i}>x</span>`;
+    }
+    deleteImg();
+}
+
+
+    
